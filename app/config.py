635f31adbb4206ebb9e6048f3f1a30b0506574a6
Tirtha Raj Poudel <tirtharajpoudel.trp@gmail.com>
2022-04-25 19:59:28 +0545

    Some refactoring

--- app/config.py
+++ app/config.py
@@ -4,9 +4,12 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     database_hostname: str
     database_port: str
-    database_password: str
-    database_name: str
+
     database_username: str
+    database_password: str
+
+    database_name: str
+
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
--- app/database.py
+++ app/database.py
@@ -8,12 +8,12 @@ SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.
 {settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
 def get_db():
-    db = session_local()
+    db = SessionLocal()
     try:
         yield db
     finally:
